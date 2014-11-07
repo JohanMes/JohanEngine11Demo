fcdf50d94559ab920246d308b31c5145fb3c924e
orwelldevcpp <orwelldevcpp@users.sourceforge.net>
2014-11-07 22:25:04 +0100

    Optimized resizing code

--- Source/JohanEngine11/JohanEngine11_private.h
+++ Source/JohanEngine11/JohanEngine11_private.h
@@ -5,19 +5,19 @@
 #define JOHANENGINE11_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.190"
+#define VER_STRING	"1.0.0.194"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	190
+#define VER_BUILD	194
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.0.190"
+#define FILE_VERSION	"1.0.0.194"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.0.190"
+#define PRODUCT_VERSION	"1.0.0.194"
 
 #endif /*JOHANENGINE11_PRIVATE_H*/
